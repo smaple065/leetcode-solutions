5ce73f251abdd015c0ac80e130d852fb2ccef00f
Sierra Maple <smaple065@gmail.com>
2023-01-29 23:05:04 -0800

    Adding missing content from 144

--- 144_binary_tree_preorder_traversal.cpp
+++ 144_binary_tree_preorder_traversal.cpp
@@ -3,3 +3,42 @@ https://leetcode.com/problems/binary-tree-preorder-traversal/
 maples1
 1/29/2023
 */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        /*
+         * Solution: Call a recursive function which pushes each node
+         * to a resulting vector. Preorder traversal is done by visiting
+         * the current node, then calling the traversal on the node 
+         * to the left, and calling the traversal on the node to the right
+        */
+        
+        vector<int> result;
+        preOrder(result, root);
+
+        return result;  
+    }
+
+        // Visit node, traverse left, traverse right
+    void preOrder(vector<int>& res, TreeNode* cur)
+    {
+        if (cur != nullptr)
+        {
+            res.push_back(cur->val);
+            preOrder(res, cur->left);
+            preOrder(res, cur->right);
+        }
+    }
+};
