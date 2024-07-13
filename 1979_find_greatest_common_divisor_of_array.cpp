c3278b0f238faf34bb63bc089504f1b4c6444e91
Sierra Maple <smaple065@gmail.com>
2024-07-13 16:37:24 -0700

    removed unneeded code for LC 1979 and further improved runtime

--- 1979_find_greatest_common_divisor_of_array.cpp
+++ 1979_find_greatest_common_divisor_of_array.cpp
@@ -14,13 +14,10 @@ public:
         /*Idea 2: Skip the first loop in favor of calling a simple sort function on nums, then grab the first and 
         last element to serve as the largest and smallest numbers*/
         
-        int length = nums.size(); // count of elements in nums
-        int gcd = 1; // greatest common denominator
-        
+        // Sort nums
         sort(nums.begin(), nums.end());
+        // Set smallest number to first and last elements if nums
         int smNum = nums[0], lgNum = nums[nums.size()-1];
-        
-        cout << "Smallest: " << smNum << " Largest: " << lgNum;
         
         // Starting at value of smNum, check if lgNum and smNum are divisible by i.
         // If neither are, move to the next largest number and check again
