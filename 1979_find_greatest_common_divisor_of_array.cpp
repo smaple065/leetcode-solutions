ce95b2ff03b7f0362aa11779b2d5633d54b870ed
Sierra Maple <smaple065@gmail.com>
2024-07-13 16:32:07 -0700

    updated solution for LC 1979 to improve runtime

--- 1979_find_greatest_common_divisor_of_array.cpp
+++ 1979_find_greatest_common_divisor_of_array.cpp
@@ -11,17 +11,16 @@ public:
         LgNum, then check the remainder of both numbers when divided by every number counting down 
         from SmNum. The first number to have a remainder of zero is the largest common divisor.*/
         
-        int smNum = nums[0], lgNum = nums[0]; // set largest and smallest to first elememt of nums
+        /*Idea 2: Skip the first loop in favor of calling a simple sort function on nums, then grab the first and 
+        last element to serve as the largest and smallest numbers*/
+        
         int length = nums.size(); // count of elements in nums
         int gcd = 1; // greatest common denominator
         
-        // Find the largest and smallest values in nums
-        for (int i = 1; i < length; i++)
-        {
-            // set new high or low
-            if (nums[i] > lgNum) lgNum = nums[i];
-            if (nums[i] < smNum) smNum = nums[i];
-        }
+        sort(nums.begin(), nums.end());
+        int smNum = nums[0], lgNum = nums[nums.size()-1];
+        
+        cout << "Smallest: " << smNum << " Largest: " << lgNum;
         
         // Starting at value of smNum, check if lgNum and smNum are divisible by i.
         // If neither are, move to the next largest number and check again
